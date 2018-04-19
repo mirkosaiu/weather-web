2dc259d738ba387402f4c094b8152f3fda466b7d
Ryuk <mirko.saiu@gmail.com>
2018-04-19 23:32:46 +0200

    api endpoint

--- src/configuration.js
+++ src/configuration.js
@@ -1,7 +1,8 @@
 
 
 const Configuration = {
-  apiEndpoint: 'http://loadBalancer-1842252216.us-west-2.elb.amazonaws.com'
+  apiEndpoint: '0.0.0.0:8080',
+  apiEndpointDev: 'http://loadBalancer-1842252216.us-west-2.elb.amazonaws.com'
 }
 
 
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,58 +3,6 @@ import Action from 'Redux/ActionNames'
 
 
 
-
-
-
-export const REQUEST_POSTS = 'REQUEST_POSTS'
-function requestPosts(subreddit) {
-  return {
-    type: REQUEST_POSTS,
-    subreddit
-  }
-}
-function fetchPosts(subreddit) {
-  return dispatch => {
-    dispatch(requestPosts(subreddit))
-    return fetch(`https://www.reddit.com/r/${subreddit}.json`)
-      .then(response => response.json())
-      .then(json => dispatch(receivePosts(subreddit, json)))
-  }
-}
-export const RECEIVE_POSTS = 'RECEIVE_POSTS'
-function receivePosts(subreddit, json) {
-  return {
-    type: RECEIVE_POSTS,
-    subreddit,
-    posts: json.data.children.map(child => child.data),
-    receivedAt: Date.now()
-  }
-}
-
-
-
-
-
-
-
-export const fetchMeasurements2 = async (dispatch) => {
-  const response = await fetch(`${Configuration.apiEndpoint}/api/measurements`)
-  const payload = await response.json()
-  dispatch({
-    type: Action.FETCH_MEASUREMENTS,
-    measurements: payload.measurements
-  })
-}
-
-
-
-
-
-
-
-
-
-
 export function fetchMeasurements() {
   return dispatch => {
     dispatch(requestMeasurements())
