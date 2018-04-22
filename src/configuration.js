2266682fc95bd52fd58a79259bb7d0d4a3eb1ca6
Ryuk <mirko.saiu@gmail.com>
2018-04-22 18:58:33 +0200

    service endpoint change

--- src/configuration.js
+++ src/configuration.js
@@ -1,7 +1,7 @@
 
 
 const Configuration = {
-  apiEndpoint: 'http://ec2-18-236-63-1.us-west-2.compute.amazonaws.com:8080'
+  apiEndpoint: 'http://WSLoadBalancer-1003950087.us-west-2.elb.amazonaws.com'
 }
 
 
