61e41777b44f9563fe242e0de3a16502b1878089
Ryuk <mirko.saiu@gmail.com>
2018-04-24 20:17:32 +0200

    added hosts

--- src/react-components/WeatherRightNow.js
+++ src/react-components/WeatherRightNow.js
@@ -3,6 +3,13 @@ import React from 'react'
 import 'Css/MeasurementsPreview.css'
 
 const WeatherRightNow = ({ temperature, luminosity, humidity, pressure, gas }) => {
+
+  var gasLevel = <span className="green-text">Low</span>
+  if (gas > 200000 ) gasLevel = <span className="orange-text">Average</span>
+  if (gas > 800000 ) gasLevel = <span className="dark-orange-text">High</span>
+  if (gas > 1500000 ) gasLevel = <span className="red-text">Very high</span>
+  if (gas > 2000000 ) gasLevel = <span className="purple-text">Very dangerous</span>
+
   return (
     <div className="measurements-preview">
       <div className="measurements-preview-titles">
@@ -17,7 +24,7 @@ const WeatherRightNow = ({ temperature, luminosity, humidity, pressure, gas }) =
         <div className="measurements-preview-item">{luminosity}</div>
         <div className="measurements-preview-item">{humidity}%</div>
         <div className="measurements-preview-item">{pressure} hPa</div>
-        <div className="measurements-preview-item">{gas}</div>
+        <div className="measurements-preview-item">{gasLevel}</div>
       </div>
     </div>
   )
