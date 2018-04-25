c3dbd376581b1a8474bc5898bfb60a5c1ef2f380
Ryuk <mirko.saiu@gmail.com>
2018-04-25 17:30:32 +0200

    new endpoint

--- src/configuration.js
+++ src/configuration.js
@@ -1,7 +1,7 @@
 
 
 const Configuration = {
-  apiEndpoint: 'http://WSLoadBalancer-2078125252.us-west-2.elb.amazonaws.com'
+  apiEndpoint: 'http://WSLoadBalancer-344095964.us-west-2.elb.amazonaws.com'
 }
 
 
